0f63e88e14d0ab35968e3b389a71d1e0d9dddb79
LuckyBuda10 <trey.buda10@gmail.com>
2024-11-15 14:19:25 -0500

    Final

---
+++ Lucky-CLI/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Can set valid commands inside this method
+        Command.SetValidCommands();
+
+        //Set up the terminal in user directory
+        CommandLine.DisplayCommandLineText();
+        FileHandler.SetDirectory(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+
+        //Main loop
+        while (true)
+        {
+            string[]? userCommand = CommandLine.GetUserCommand();
+
+            //Reprompt the user if they put an invalid command
+            if (Command.CheckInvalidCommand(userCommand!))
+                continue;
+
+            //It won't be null, it's checked in above function
+            string commandName = userCommand![0];
+            Command command = Command.GetCommandInfo(commandName)!;
+
+            command.Run(userCommand.ToList());
+        }
+    }
+}
